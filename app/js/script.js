68fb6c2b6a9a54249a95e6cbbb95aa25346a7a81
ricky <ricky.chaggar@gmail.com>
2021-08-23 16:23:45 +0100

    added js function and scss code in header to effect transition between humbuger menu and cross when clicked

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello, world");
+const btnhamburger = document.querySelector("#btnhamburger");
+
+btnhamburger.addEventListener("click", (e) => {
+  console.log("clicked " + e);
+
+  if (btnhamburger.classList.contains("open")) {
+    btnhamburger.classList.remove("open");
+  } else {
+    btnhamburger.classList.add("open");
+  }
+});
