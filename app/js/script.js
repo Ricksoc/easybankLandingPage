e489094dad342f8570919c6270efba241bada5d5
ricky <ricky.chaggar@gmail.com>
2021-08-24 16:14:42 +0100

    added fade-in and fade-out keyframe animations to overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,17 @@
 const btnhamburger = document.querySelector("#btnhamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".fade");
 
 header.addEventListener("click", (e) => {
   console.log("clicked " + e);
 
   header.classList.toggle("open");
+
+  if (header.classList.contains("open")) {
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  } else {
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  }
 });
