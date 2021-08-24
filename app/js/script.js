63761dacc99e91fdccb27235491e02df19829810
ricky <ricky.chaggar@gmail.com>
2021-08-24 08:53:41 +0100

    switch js hamburger menu to classList.toggle to remove need for if logic check

--- app/js/script.js
+++ app/js/script.js
@@ -3,9 +3,5 @@ const btnhamburger = document.querySelector("#btnhamburger");
 btnhamburger.addEventListener("click", (e) => {
   console.log("clicked " + e);
 
-  if (btnhamburger.classList.contains("open")) {
-    btnhamburger.classList.remove("open");
-  } else {
-    btnhamburger.classList.add("open");
-  }
+  btnhamburger.classList.toggle("open");
 });
