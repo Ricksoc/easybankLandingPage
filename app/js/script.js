f44c2895caff2e0ac744fadd10d134842c667515
ricky <ricky.chaggar@gmail.com>
2021-08-25 16:32:10 +0100

    added noscroll when hamburger menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnhamburger = document.querySelector("#btnhamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const fades = document.querySelectorAll(".fade");
 
@@ -9,12 +10,14 @@ btnhamburger.addEventListener("click", (e) => {
 
   if (header.classList.contains("open")) {
     // open Hamburger menu
+    body.classList.add("noscroll");
     fades.forEach((obj) => {
       obj.classList.remove("fade-out");
       obj.classList.add("fade-in");
     });
   } else {
     // Close Hamburger menu
+    body.classList.remove("noscroll");
     fades.forEach((obj) => {
       obj.classList.remove("fade-in");
       obj.classList.add("fade-out");
