01425da1136c701ddc6a4131facbc2ac883b214e
ricky <ricky.chaggar@gmail.com>
2021-09-08 14:57:21 +0100

    added fade-out / in to hero__image so that it disappears when buger menu is opened

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,8 @@
 const btnhamburger = document.querySelector("#btnhamburger");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
-const fades = document.querySelectorAll(".fade");
+const fadesIns = document.querySelectorAll(".fade");
+const fadeOuts = document.querySelectorAll(".outFade");
 
 btnhamburger.addEventListener("click", (e) => {
   console.log("hamburger clicked");
@@ -11,16 +12,26 @@ btnhamburger.addEventListener("click", (e) => {
   if (header.classList.contains("open")) {
     // open Hamburger menu
     body.classList.add("noscroll");
-    fades.forEach((obj) => {
+    fadesIns.forEach((obj) => {
       obj.classList.remove("fade-out");
       obj.classList.add("fade-in");
+    });
+
+    fadeOuts.forEach((obj) => {
+      obj.classList.remove("fade-in");
+      obj.classList.add("fade-out");
     });
   } else {
     // Close Hamburger menu
     body.classList.remove("noscroll");
-    fades.forEach((obj) => {
+    fadesIns.forEach((obj) => {
       obj.classList.remove("fade-in");
       obj.classList.add("fade-out");
-    });
+    })
+    
+    fadeOuts.forEach((obj) => {
+      obj.classList.remove("fade-out");
+      obj.classList.fade("fade-in");
+    });;
   }
 });
